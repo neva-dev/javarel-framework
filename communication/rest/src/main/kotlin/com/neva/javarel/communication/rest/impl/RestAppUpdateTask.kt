63d1b247710e6e7ded9c20c448fed9c79e1b6f2a
Punky <krystianpanek@neva.zone>
2016-03-09 12:00:26 +0100

    Http service updater works

--- communication/rest/src/main/kotlin/com/neva/javarel/communication/rest/impl/RestAppUpdateTask.kt
+++ communication/rest/src/main/kotlin/com/neva/javarel/communication/rest/impl/RestAppUpdateTask.kt
@@ -1,11 +1,11 @@
 package com.neva.javarel.communication.rest.impl
 
 import com.neva.javarel.communication.rest.api.RestApplication
-import com.neva.javarel.foundation.osgi.ServiceUtils
 import com.neva.javarel.processing.scheduler.api.Task
 import org.apache.felix.ipojo.annotations.Component
 import org.apache.felix.ipojo.annotations.Instantiate
 import org.apache.felix.ipojo.annotations.Provides
+import org.apache.felix.ipojo.annotations.Requires
 import org.quartz.*
 import org.slf4j.LoggerFactory
 
@@ -17,6 +17,11 @@ class RestAppUpdateTask : Task {
     companion object {
         val logger = LoggerFactory.getLogger(RestAppUpdateTask::class.java)
     }
+
+    @Requires
+    lateinit var restApp: RestApplication
+
+    private var componentCount = -1
 
     private val trigger = TriggerBuilder
             .newTrigger()
@@ -32,13 +37,22 @@ class RestAppUpdateTask : Task {
     }
 
     override fun getJob(): JobDetail {
+        job.jobDataMap.put(RestAppUpdateTask::class.simpleName, this);
+
         return job
     }
 
     class UpdateJob : Job {
+
         override fun execute(context: JobExecutionContext?) {
-            // TODO fix getService() == null, or pass injected restApplication somehow, update only component count changed
-            ServiceUtils.getService(RestApplication::class).updateHttpService()
+            val task = context!!.jobDetail.jobDataMap.get(RestAppUpdateTask::class.simpleName) as RestAppUpdateTask;
+
+            if (task.componentCount == -1 || task.restApp.getComponents().size != task.componentCount) {
+                task.componentCount = task.restApp.getComponents().size
+
+                logger.info("Updating rest application")
+                task.restApp.updateHttpService()
+            }
         }
     }
 }
